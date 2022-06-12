f1cd26b783c56fb4010039c170fbb37a739be500
diego2317 <98351752+diego2317@users.noreply.github.com>
2022-06-12 09:24:32 -0400

    Delete button2.py

--- button2.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Button(pygame.sprite.Sprite):
-    def __init__(self, picture_path, pos_x, pos_y):
-        super().__init__()
-        self.image = pygame.load(picture_path)
-        self.rect = self.image.get_rect()
-        self.rect.center = [pos_x, pos_y]
